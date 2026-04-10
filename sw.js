b6fc9b5fb6b6380d0f68f1dce385686a6a574355
mahbubtopuca-design <mahbubtopu.ca@gmail.com>
2026-04-10 15:08:51 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dokan-pro-v9';
+const CACHE_NAME = 'dokan-pro-v10';
 const ASSETS = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
